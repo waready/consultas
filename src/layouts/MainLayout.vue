e29655ba7787280c5bda229a454ed5dd94c37b93
waready <elbarto119@hotmail.com>
2024-01-25 01:30:35 -0500

    primer commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          App Validacion
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/" label="DNI" />
+        <q-route-tab to="/page2" label="RUC" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar class="text-caption">
+          <div>WAREADY © 2024 </div>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input
+      ref="myInput"
+      standout
+      v-model="text"
+      :dense="dense"
+      :maxlength="8"
+      type="number"
+      :rules="[
+        (value) => !!value || 'Este campo es obligatorio',
+        (value) => /^\d+$/.test(value) || 'Debe contener solo números',
+        (value) =>
+          (value && value.length === 8) || 'Debe tener 8 dígitos numéricos',
+      ]"
+      @input="handleInput"
+    >
+      <template v-slot:append>
+        <q-avatar>
+          <q-icon name="person" class="text-primary" style="font-size: 30px" />
+        </q-avatar>
+      </template>
+    </q-input>
+
+    <div class="q-pa-md q-gutter-sm">
+      <q-dialog v-model="seamless" seamless position="bottom">
+        <q-card style="width: 350px; height: 150px">
+          <q-linear-progress :value="1" color="blue" />
+
+          <q-card-section class="row items-center no-wrap">
+            <div>
+              <div class="text-h6 text-grey">Nombre Completo:</div>
+              <div class="text-h5 text-weight-bold">
+                <div v-if="!fetchDataResult">cargando ...</div>
+                <div v-else-if="fetchDataResult.nombre"> {{ fetchDataResult.nombre }}</div>
+                <div v-else>
+                  No existe o es menor de edad 😡
+                </div>
+                
+              </div>
+            </div>
+
+            <q-space />
+            <q-btn flat round icon="close" v-close-popup />
+          </q-card-section>
+        </q-card>
+      </q-dialog>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import axios from "axios";
+import { QSpinnerGears } from 'quasar';
+
+export default defineComponent({
+  name: "IndexPage",
+  data() {
+    return {
+      text: "",
+      dense: false,
+      seamless: false,
+      fetchDataResult: "", // Variable para almacenar el resultado de fetchData
+    };
+  },
+  mounted() {
+    // Dando focus al q-input al cargar la página
+    this.$refs.myInput.focus();
+  },
+  watch: {
+    text(newText) {
+      if (newText.length === 8) {
+        this.seamless = true;
+        this.fetchData();
+      }
+    },
+  },
+  methods: {
+    async fetchData() {
+      try {
+
+        const dismiss = this.$q.notify({
+            spinner: QSpinnerGears,
+            message: '<p>Esperando al Servidor &nbsp;<span><i class="fa fa-server" aria-hidden="true"></i> <span> <p>',
+            timeout: 0, // Establecer el timeout a 0 para que la notificación no se cierre automáticamente
+            html: true
+        });
+        const response = await axios.get(
+          `https://afocatpuno.com/api/v1/getdni?valor=${this.text}`
+        );
+        this.fetchDataResult = response.data;
+        dismiss() // Asignar el resultado a la variable
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  },
+});
+</script>
